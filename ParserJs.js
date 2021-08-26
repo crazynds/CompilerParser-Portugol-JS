b5c04cad7e29c4976e5df79c16bdb8b372f39d3e
crazynds <luli.broch@gmail.com>
2021-08-26 20:22:43 -0300

    adicionado alguns ;

--- ParserJs.js
+++ ParserJs.js
@@ -3,12 +3,12 @@ const AnalisadorSintatico = require('./AnalisadorSintatico');
 const AnalisadorSemanatico = require('./AnalisadorSemantico');
 const gramatica = require('./Gramatica');
 
-var lexico = AnalisadorLexico.start('r1.txt')
+var lexico = AnalisadorLexico.start('r1.txt');
 
 
 console.log('Carregando gramatica...');
 var matrizDaGramatica = AnalisadorSintatico.criarGramatica(gramatica);
-console.log('Gramatica carregada!')
+console.log('Gramatica carregada!');
 
 
 var arvore = AnalisadorSemanatico.analisaGramatica(lexico,matrizDaGramatica);
